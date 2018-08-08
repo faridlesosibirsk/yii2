caacb28578909fcd24d361c05a50eda14b2560be
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2018-08-08 15:49:24 +0700

    init commit

---
+++ models/my/Country.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace app\models\my;
+
+use yii\db\ActiveRecord;
+
+class Country extends ActiveRecord {
+    
+}
---
+++ models/my/Payment.php
@@ -0,0 +1,17 @@
+<?php
+
+//namespace app\models\my;
+
+class Payment {
+
+    private $amount = 0;
+
+    public function Payment($cashTendered) {
+        $this->$amount = $cashTendered;
+    }
+
+    public function getAmount() {
+        return $this->amount;
+    }
+
+}
